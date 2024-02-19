f1f7d5e4c1be46cec4870c43b62e1f9e53a40412
CHOHUNE <yuureru@gmail.com>
2024-02-19 12:04:17 +0900

    first commit

---
+++ src/test/java/hello/servlert/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
