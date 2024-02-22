fa721c8809f8b005e3a582e2fdaa04eb71f00fa9
CHOHUNE <yuureru@gmail.com>
2024-02-22 16:57:19 +0900

    서블릿,JSP, MVC 패턴-
    프론트 컨트롤러 -Model 추가

--- src/main/java/hello/servlert/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlert/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -19,47 +19,37 @@ import java.util.Map;
 public class FrontControllerServletV3 extends HttpServlet {
 
     private Map<String, ControllerV3> controllerMap = new HashMap<>();
-
     public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        controllerMap.put("/front-controller/v3/members/new-form", new
+                MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new
+                MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new
+                MemberListControllerV3());
     }
-
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void service(HttpServletRequest request, HttpServletResponse
+            response)
+            throws ServletException, IOException {
         String requestURI = request.getRequestURI();
-
         ControllerV3 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        //paramMap을 넘겨줘야 한다.
-        // 이터레이터로 파람 맵을 다 꺼내야 한다.
+            return; }
         Map<String, String> paramMap = createParamMap(request);
-
         ModelView mv = controller.process(paramMap);
-
-        String viewName = mv.getViewName();//논리이름만 얻고 있어 (new-form) 물리 이름으로 변경 해주는 과정이 필요하다
+        String viewName = mv.getViewName();
         MyView view = viewResolver(viewName);
-
-
         view.render(mv.getModel(), request, response);
-
     }
-
-    private static MyView viewResolver(String viewName) {
-        return new MyView("/WEB_INF/views/" + viewName + "jsp");
-    }
-
-    private static Map<String, String> createParamMap(HttpServletRequest request) {
+    private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
         request.getParameterNames().asIterator()
-                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+                .forEachRemaining(paramName -> paramMap.put(paramName,
+                        request.getParameter(paramName)));
         return paramMap;
     }
-
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
 }
