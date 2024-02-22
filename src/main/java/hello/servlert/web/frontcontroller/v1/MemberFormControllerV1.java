84d3ec44593baebf67155fffae83715a5101549f
CHOHUNE <yuureru@gmail.com>
2024-02-22 14:07:30 +0900

    서블릿,JSP, MVC 패턴-
    프론트 컨트롤러

--- src/main/java/hello/servlert/web/frontcontroller/v1/MemberFormControllerV1.java
+++ src/main/java/hello/servlert/web/frontcontroller/v1/MemberFormControllerV1.java
@@ -1,30 +1,23 @@
-package hello.servlert.web.servletmvc;
+package hello.servlert.web.frontcontroller.v1;
 
-
-import hello.servlert.basic.HelloServlet;
+import hello.servlert.web.frontcontroller.ControllerV1;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name="mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
-public class MvcMemberFormServlet extends HttpServlet {
+public class MemberFormControllerV1 implements ControllerV1 {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String viewPath = "/WEB-INF/views/new-form.jsp";
         // 컨트롤러는 단순히 JSP로 가주기만 하면 된다 ( 뷰 )
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         // 컨트롤러 - > 뷰 에서 이동할 때 사용하는 메서드
         dispatcher.forward(request,response);
-
-
 
     }
 }
