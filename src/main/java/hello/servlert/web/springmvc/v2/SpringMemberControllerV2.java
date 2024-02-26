279d6074cfeb0926682e3d8bcdda459634959215
CHOHUNE <yuureru@gmail.com>
2024-02-26 16:11:39 +0900

    스프링 MVC- 구조 이해 - 컨트롤러 통합

---
+++ src/main/java/hello/servlert/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,55 @@
+package hello.servlert.web.springmvc.v2;
+
+
+import hello.servlert.domain.member.Member;
+import hello.servlert.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        // 이전에 해왔 듯 HttpServletRequest.getParametr()로 꺼내는 게 아니라 X
+        // 위의 정보는 프론트 컨트롤러에서 처리하고
+        // 파람 맵에다가 요청 파라메터 정보를 전부 담아 넘겨줄 것이다.
+        // 여기서는 단순히 꺼내서 쓰기만 하면 된다.
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+
+
+}
